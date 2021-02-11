89a5e70d7fb3b688b3929d3523ae1e4d20bfe21a
marcvernet31 <44305244+marcvernet31@users.noreply.github.com>
2021-02-11 19:40:11 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import praw
 import pprint
 import pandas as pd
-from datetime import datetime
 
 import argparse
 import time
@@ -11,12 +10,18 @@ from secrets import CLIENT_ID, CLIENT_SECRET, USER_AGENT
 from redditScrap import retrieveComments, retrievePosts
 
 """
-PER FER:
-    - Comentar tot ben bonic
-    
-COSES DEL FUTUR:
-    - Poder filtrar per temps, incorporar el temps al nom del fitxer
-    - Permetre entrar un enllaç web i descarregar tots els comentaris del post
+Main script for the Reddit Scrapper project.
+--------------------------------------------------------------------------------
+This script provides a way to download interactively from a terminal posts and
+comments from Reddit submissions. The aim and vision of this project is to be
+able to easily download massive quantities of text from Reddit for future ML and
+data science analysis.
+
+
+TO BE DONE:
+    - Adding the capability to filter by timestamp and updating files by timestamp
+    - Adding the capability of inputinc a web link of a Reddit post to download
+    its comments.
 """
 
 
@@ -30,10 +35,9 @@ parser.add_argument("-cret", "--commentsRetrieved", help="Number of root posts f
 parser.add_argument("-u", "--update", help="True to update a data file")
 parser.add_argument("-sf", "--savingFrequency", help="Size of batches to be saved")
 
-
 args = parser.parse_args()
 
-# Parametres dels flags
+# Definition of flag parameters
 def flag_parameters():
     if args.replaceLimit: replaceLimit = args.replaceLimit
     else: replaceLimit = 1
@@ -60,50 +64,53 @@ def flag_parameters():
 
 def main():
 
+    # Retrieve values from flags
     replaceLimit, getComments, commentSort, commentsRetrieved, update, savingFrequency = flag_parameters()
+
+    # Dictionary for storing submissions already uploaded
+    # (for update functionality)
     isRetrieved = dict()
 
-
+    # Ask and get user input.
     print("------------------------------------------")
     input_subreddit = input("Enter Subreddit to search: ")
     submissionsRetrieved = input("Enter number of Submissions to retireve: ")
-
     try:
         submissionsRetrieved = int(submissionsRetrieved)
     except ValueError:
         print("Error: Not a number")
         submissionsRetrieved = input("Enter number of Submissions to retireve: ")
         submissionsRetrieved = int(submissionsRetrieved)
-
     if update:
         postPath = input("Path of the post file: ")
         commentPath = input("Path of the comment file: ")
-
         try:
             tmp_dfPost = load_DataFrame(postPath, "posts")
             tmp_dfComment = load_DataFrame(commentPath, "comments")
             isRetrieved = dict.fromkeys(list(tmp_dfPost["ID"]), True)
         except:
             print("Error with file names")
-
     print("------------------------------------------")
 
-
+    # Initialize Reddit object and specified subreddit
     reddit = praw.Reddit(client_id = CLIENT_ID, client_secret = CLIENT_SECRET, user_agent= USER_AGENT, check_for_async=False)
     subreddit = reddit.subreddit(input_subreddit)
 
-
+    # Retrieve data from Reddit
     start = time.time()
     dfPost, dfComment = retrievePosts(subreddit, submissionsRetrieved, isRetrieved, getComments, commentSort, commentsRetrieved, savingFrequency, replaceLimit)
     end = time.time()
 
+    # Append to old data in case of update
     if update:
         dfPost = dfPost.append(tmp_dfPost)
         dfComment = dfComment.append(tmp_dfComment)
 
+    # Save final result
     save(dfPost, f"data/{subreddit.display_name}_posts.csv")
     if getComments: save(dfComment, f"data/{subreddit.display_name}_comments.csv")
 
+    # Printing of final results and metrics
     print("------------------------------------------")
     print("Extraction finished, result stored in: ")
     print(f"- Posts: data/{subreddit.display_name}_posts.csv")
